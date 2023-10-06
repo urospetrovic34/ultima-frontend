b31f7680aff16dff949ddb255daea39515acaf6d
uros.petrovic <uros.petrovic@quantox.com>
2023-10-07 01:53:06 +0200

    Added Error Not Found Page

---
+++ error.vue
@@ -0,0 +1,7 @@
+<template>
+  <NuxtLayout>
+    <div class="flex items-center justify-center" :style="{ height: '100%' }">
+      It seems that this page doesn't exist, yet.
+    </div>
+  </NuxtLayout>
+</template>
